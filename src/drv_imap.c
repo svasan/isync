0a5a84793283bae245d64fbf634f651b433afe0d
Oswald Buddenhagen <ossi@users.sf.net>
2017-11-18 09:57:05 +0100

    fix IMAP UID sequence in UIDNEXT determination fallback
    use just * instead of the rather nonsensical *:* (which davmail happens
    to actually barf at).
    
    amends 72be55b0.

--- src/drv_imap.c
+++ src/drv_imap.c
@@ -2388,7 +2388,7 @@ imap_open_box_p2( imap_store_t *ctx, imap_cmd_t *gcmd, int response )
 	INIT_IMAP_CMD(imap_cmd_open_box_t, cmd, cmdp->callback, cmdp->callback_aux)
 	cmd->gen.param.lastuid = 1;
 	imap_exec( ctx, &cmd->gen, imap_open_box_p3,
-	           "UID FETCH *:* (UID)" );
+	           "UID FETCH * (UID)" );
 }
 
 static void
